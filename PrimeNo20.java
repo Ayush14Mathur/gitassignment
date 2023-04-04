2c53e68552bfcd7994761f9df1303924771f4374
Ayush14Mathur <128884030+Ayush14Mathur@users.noreply.github.com>
2023-04-04 14:11:40 +0530

    Update PrimeNo20.java

--- PrimeNo20.java
+++ PrimeNo20.java
@@ -3,10 +3,10 @@ package lab1;
 public class PrimeNo20 {
 
 	public static void main(String[] args) {
-		System.out.println("Prime numbers between 2 to 20");
-				for(int i=2;i<=20;i++)
+		System.out.println("Prime numbers between 2 to 30");
+				for(int i=2;i<=30;i++)
 				{
-					for(int j=2;i<=20;i++)
+					for(int j=2;i<=30;i++)
 						if(i!=j&&i%j!=0)
 						{
 							System.out.print(i);
